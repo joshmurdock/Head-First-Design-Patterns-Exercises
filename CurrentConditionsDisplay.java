43c7e0e544fb5ef2c66354cd620b6ad8c09518cb
Josh <jmurd12@proton.me>
2024-06-01 12:28:10 -0400

    Oberserver pattern

---
+++ CurrentConditionsDisplay.java
@@ -0,0 +1,26 @@
+public class CurrentConditionsDisplay implements Observer, Display {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    private WeatherData weatherData;
+
+    public CurrentConditionsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update() {
+        this.temperature = weatherData.getTemperature();
+        this.humidity = weatherData.getHumidity();
+        this.pressure = weatherData.getPressure();
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current Conditions: " + temperature + 
+        "F degrees and " + humidity + "% humidity and " + pressure + " pressure");
+    }
+}
+
